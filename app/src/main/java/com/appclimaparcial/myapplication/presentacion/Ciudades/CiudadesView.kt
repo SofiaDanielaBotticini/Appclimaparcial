693ef2a1bd90078fa52295ef8f6b3417040313aa
Lucía S <lschiaffi@gmail.com>
2025-11-18 01:00:08 -0300

    ListadeCiudades UI updated

--- app/src/main/java/com/appclimaparcial/myapplication/presentacion/Ciudades/CiudadesView.kt
+++ app/src/main/java/com/appclimaparcial/myapplication/presentacion/Ciudades/CiudadesView.kt
@@ -3,6 +3,7 @@ package com.appclimaparcial.myapplication.presentacion.Ciudades
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,25 +15,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.appclimaparcial.myapplication.repository.modelos.Ciudad
 import kotlinx.serialization.InternalSerializationApi
 
 @OptIn(InternalSerializationApi::class)
 @Composable
 fun CiudadesView(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp, vertical = 4.dp),
     state: CiudadesEstado,
     onAction: (CiudadesIntencion) -> Unit,
     onRequestLocation: () -> Unit // Es para que la vista pida permiso/ubicación al usuario
 ) {
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = { TopBar() })
-    { innerPadding ->
+        modifier = modifier.fillMaxSize(), topBar = { TopBar() }) { innerPadding ->
         var value by remember { mutableStateOf("") }
 
         Column(modifier = modifier.padding(innerPadding)) {
@@ -44,10 +49,12 @@ fun CiudadesView(
                     value = it
                     onAction(CiudadesIntencion.Buscar(value))
                 },
-                modifier = Modifier.fillMaxWidth()
+                modifier = modifier
             )
 
-            Button(onClick = { onRequestLocation() }) {
+            Button(
+                onClick = { onRequestLocation() }, modifier = modifier.height(40.dp)
+            ) {
                 Text(text = "Buscar por geolocalización")
             }
 
@@ -55,35 +62,38 @@ fun CiudadesView(
                 is CiudadesEstado.Recomendadas -> {
                     Text(
                         "Ciudades recomendadas:",
-                        style = MaterialTheme.typography.titleMedium
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = modifier.height(40.dp)
                     )
-                    ListaDeCiudades(state.ciudades) { ciudad ->
+                    ListaDeCiudades(state.ciudades, modifier) { ciudad ->
                         onAction(CiudadesIntencion.Seleccionar(ciudad))
                     }
                 }
 
-                CiudadesEstado.Cargando -> Text("Cargando...")
+                CiudadesEstado.Cargando -> Text("Cargando...", modifier = modifier)
 
-                is CiudadesEstado.Error -> Text(state.mensaje)
+                is CiudadesEstado.Error -> Text(state.mensaje, modifier = modifier)
 
                 is CiudadesEstado.Resultado -> {
-                    ListaDeCiudades(state.ciudades) {
+                    ListaDeCiudades(state.ciudades, modifier) {
                         onAction(CiudadesIntencion.Seleccionar(it))
                     }
                 }
 
-                CiudadesEstado.Vacio -> Text("No hay resultados")
+                CiudadesEstado.Vacio -> Text("No hay resultados", modifier = modifier)
             }
         }
     }
 }
 
 @Composable
-fun ListaDeCiudades(ciudades: List<Ciudad>, onSelect: (Ciudad) -> Unit) {
+fun ListaDeCiudades(ciudades: List<Ciudad>, modifier: Modifier, onSelect: (Ciudad) -> Unit) {
     LazyColumn {
         items(items = ciudades) {
-            Card(onClick = { onSelect(it) }) {
-                Text(text = it.name)
+            Card(
+                onClick = { onSelect(it) }, modifier = modifier.height(40.dp)
+            ) {
+                Text(text = it.name, modifier = modifier.padding(8.dp))
             }
         }
     }
--- app/src/main/java/com/appclimaparcial/myapplication/repository/RepositorioMock.kt
+++ app/src/main/java/com/appclimaparcial/myapplication/repository/RepositorioMock.kt
@@ -1,58 +1,57 @@
-/*package com.appclimaparcial.myapplication.repository
-
-import com.appclimaparcial.myapplication.repository.modelos.Ciudad
-import com.appclimaparcial.myapplication.repository.modelos.Clima
-import com.appclimaparcial.myapplication.repository.modelos.ListForecast
-
-class RepositorioMock  : Repositorio {
-
-    val cordoba = Ciudad(
-        name = "Cordoba",
-        lat = -31.42f,
-        lon = -64.18f,
-        country = "Argentina"
-    )
-    val bsAs = Ciudad(
-        name = "Buenos Aires",
-        lat = -34.61f,
-        lon = -58.38f,
-        country = "Argentina"
-    )
-    val laPlata = Ciudad(
-        name = "La Plata",
-        lat = -34.92f,
-        lon = -57.95f,
-        country = "Argentina")
-
-    val ciudades = listOf(cordoba,bsAs,laPlata)
-
-    override suspend fun buscarCiudad(ciudad: String): List<Ciudad> {
-        return when {
-            ciudad.lowercase().contains("cor") -> listOf(cordoba)
-            ciudad.lowercase().contains("plata") -> listOf(laPlata)
-            else -> emptyList()
-        }
-    }
-
-    override suspend fun buscarCiudadPorCoords(lat: Float, lon: Float): List<Ciudad> {
-        return when {
-            lat == cordoba.lat && lon == cordoba.lon -> listOf(cordoba)
-            lat == laPlata.lat && lon == laPlata.lon -> listOf(laPlata)
-            else -> emptyList()
-        }
-    }
-
-    override suspend fun traerClima(lat: Float, lon: Float): Clima {
-    }
-
-    override suspend fun traerPronostico(nombre: String): List<ListForecast> {
-    }
-}
-
-class RepositorioMockError  : Repositorio {
-    override suspend fun buscarCiudad(ciudad: String): List<Ciudad> { throw Exception() }
-    override suspend fun buscarCiudadPorCoords(lat: Double, lon: Double): List<Ciudad> { throw Exception() }
-    override suspend fun traerClima(lat: Float, lon: Float): Clima { throw Exception() }
-    override suspend fun traerPronostico(nombre: String): List<ListForecast> { throw Exception() }
-}
-*/
+//package com.appclimaparcial.myapplication.repository
+//
+//import com.appclimaparcial.myapplication.repository.modelos.Ciudad
+//import com.appclimaparcial.myapplication.repository.modelos.Clima
+//import com.appclimaparcial.myapplication.repository.modelos.ListForecast
+//
+//class RepositorioMock  : Repositorio {
+//
+//    val cordoba = Ciudad(
+//        name = "Cordoba",
+//        lat = -31.42f,
+//        lon = -64.18f,
+//        country = "Argentina"
+//    )
+//    val bsAs = Ciudad(
+//        name = "Buenos Aires",
+//        lat = -34.61f,
+//        lon = -58.38f,
+//        country = "Argentina"
+//    )
+//    val laPlata = Ciudad(
+//        name = "La Plata",
+//        lat = -34.92f,
+//        lon = -57.95f,
+//        country = "Argentina")
+//
+//    val ciudades = listOf(cordoba,bsAs,laPlata)
+//
+//    override suspend fun buscarCiudad(ciudad: String): List<Ciudad> {
+//        return when {
+//            ciudad.lowercase().contains("cor") -> listOf(cordoba)
+//            ciudad.lowercase().contains("plata") -> listOf(laPlata)
+//            else -> emptyList()
+//        }
+//    }
+//
+//    override suspend fun buscarCiudadPorCoords(lat: Float, lon: Float): List<Ciudad> {
+//        return when {
+//            lat == cordoba.lat && lon == cordoba.lon -> listOf(cordoba)
+//            lat == laPlata.lat && lon == laPlata.lon -> listOf(laPlata)
+//            else -> emptyList()
+//        }
+//    }
+//
+//    override suspend fun traerClima(lat: Float, lon: Float): Clima {
+//    }
+//
+//    override suspend fun traerPronostico(nombre: String): List<ListForecast> {
+//    }
+//}
+//
+//class RepositorioMockError  : Repositorio {
+//    override suspend fun buscarCiudad(ciudad: String): List<Ciudad> { throw Exception() }
+//    override suspend fun buscarCiudadPorCoords(lat: Double, lon: Double): List<Ciudad> { throw Exception() }
+//    override suspend fun traerClima(lat: Float, lon: Float): Clima { throw Exception() }
+//    override suspend fun traerPronostico(nombre: String): List<ListForecast> { throw Exception() }
+//}
